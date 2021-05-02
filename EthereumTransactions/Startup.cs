4b6876e2f53ce712f42b5b328ecaddb8493e11d8
Arvind Anbuvel <arvinda@openpay.com.au>
2021-05-02 22:10:31 +1000

    Checkin cors config. Sorry forgot to push this in

--- EthereumTransactions/Startup.cs
+++ EthereumTransactions/Startup.cs
@@ -54,6 +54,12 @@ namespace EthereumTransactions
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			app.UseCors(
+				options => options.AllowAnyOrigin()
+								  .AllowAnyHeader()
+								  .AllowAnyMethod()
+			);
+
 			// Enable middleware to serve generated Swagger as a JSON endpoint.
 			app.UseSwagger();
 
