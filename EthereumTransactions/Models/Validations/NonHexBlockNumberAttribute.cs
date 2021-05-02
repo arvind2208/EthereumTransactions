a63c2f132f627acd89add0627187381c96facbb2
Arvind Anbuvel <arvinda@openpay.com.au>
2021-05-03 01:33:36 +1000

    remove try catch

--- EthereumTransactions/Models/Validations/NonHexBlockNumberAttribute.cs
+++ EthereumTransactions/Models/Validations/NonHexBlockNumberAttribute.cs
@@ -7,16 +7,10 @@ namespace EthereumTransactions.Models.Validations
 	{
 		public override bool IsValid(object value)
 		{
-			try
-			{
-				var blockNumber = Convert.ToInt64(value);
-				if ($"{blockNumber:X2}".Length == 6)
-					return true;
-			}
-			catch
-			{
-				return false;
-			}
+			var blockNumber = Convert.ToInt64(value);
+			if ($"{blockNumber:X2}".Length == 6)
+				return true;
+
 			return false;
 		}
 	}
